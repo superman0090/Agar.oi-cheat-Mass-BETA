da81f401d51543c0c818ab834fc63caa38209e22
Lefela4 <felixantoinebelleau@gmail.com>
2015-08-20 11:59:02 -0400

    Update AgarMassConfig.user.js

--- AgarMassConfig.user.js
+++ AgarMassConfig.user.js
@@ -8,22 +8,22 @@
 // @include      http://agar.io/*
 // ==/UserScript==
 
-<!DOCTYPE html>
-<html>
-<body>
+// ==UserScript==
+// @name         Agar.io connector.
+// @namespace    http://msparp.com/
+// @version      0.2
+// @description  Allows you to manually enter a server IP.
+// @author       capableResistor
+// @match        http://agar.io/
+// @grant        none
+// ==/UserScript==
 
-Name: <input type="text" id="myText" value="100">
-
-<p>Click the button to change the value of the text field.</p>
-
-<button onclick="myFunction()">Mass</button>
-
-<script>
-function myFunction() {
-    document.getElementById("myText").value = "100";
-}
-</script>
-
-</body>
+$(document).ready(function() {
+    var region = $("#region");
+    if (region.length) {
+        $("<div class=\"form-group\"><input id=\"serverInput\" class=\"form-control\" placeholder=\"127.0.0.1:4545\" maxlength=\"20\"></input></div>").insertAfter("#helloDialog > form > div:nth-child(3)");
+        $("<div class=\"form-group\"><button disabled type=\"button\" id=\"connectBtn\" class=\"btn btn-warning btn-needs-server\" onclick=\"connect('ws://' + $('#serverInput').val());\" style=\"width: 100%\">Connect</button></div>").insertAfter($("#serverInput").parent());
+    }
+});
 </html>
 
