1c5b56fba8d13647a77c4dbf5a534ec50d264e6b
Lefela4 <felixantoinebelleau@gmail.com>
2015-08-20 13:29:15 -0400

    Update servers.user.js

--- servers.user.js
+++ servers.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         AgarMassConfig
-// @namespace    AgarMassCinfig
+// @name         servers
+// @namespace    servers
 // @version      1
 // @grant       none
 // @description  AgarMass Cheat Config
