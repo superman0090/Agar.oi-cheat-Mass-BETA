c18a0e3297cc89592bce5b1c8ff06329aa7c25d5
Lefela4 <felixantoinebelleau@gmail.com>
2015-08-20 12:57:13 -0400

    Update AgarMassConfig.user.js

--- AgarMassConfig.user.js
+++ AgarMassConfig.user.js
@@ -8,4 +8,19 @@
 // @include      http://agar.io/*
 // ==/UserScript==
 
-connect("ws://192.168.1.81:4545");
+var connection = new WebSocket('ws://192.168.1.81:4545');
+
+// When the connection is open, send some data to the server
+connection.onopen = function () {
+  connection.send('Ping'); // Send the message 'Ping' to the server
+};
+
+// Log errors
+connection.onerror = function (error) {
+  console.log('WebSocket Error ' + error);
+};
+
+// Log messages from the server
+connection.onmessage = function (e) {
+  console.log('Server: ' + e.data);
+};
